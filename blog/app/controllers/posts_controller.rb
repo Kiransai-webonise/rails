9c9fdd9adb20125bca39799a0df5053a8151c78e
Kiransai-webonise <kiran.sai@weboniselab.com>
2022-08-03 11:35:13 +0530

    ODM

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_url
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    @post.update(post_params)
+    redirect_to posts_url
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy
+    redirect_to posts_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :body, :status)
+    end
+end
