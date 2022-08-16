dda7891924fdccb0f8e176e1c6dfbe9861a29061
Kiransai-webonise <kiran.sai@weboniselab.com>
2022-08-16 20:58:50 +0530

    addressed comments

--- microblog/config/routes.rb
+++ microblog/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :blogs
   put 'blogs/change_status/:id', to: 'blogs#change_status', as: 'change_status'
 
-  mount Articles::Engine, at: "/types"
+  mount Articles::Engine, at: "/static_pages"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
